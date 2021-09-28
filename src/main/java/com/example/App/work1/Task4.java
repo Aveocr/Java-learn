c7c01e5faeb2a3eff2857981ed4da89d5efb849f
Aveocr <aveocr@yandex.ru>
2021-09-28 22:46:31 +0400

    задание 4

--- src/main/java/com/example/App/work1/Task4.java
+++ src/main/java/com/example/App/work1/Task4.java
@@ -4,7 +4,7 @@ public class Task4 {
     public static void main(String[] argc) {
         Task3 task = new Task3();
         int sum = 0;
-        for (int i = 1000; i < 2000; i++) {
+        for (int i = 1000; i <= 2000; i++) {
             if (task.checkPrimeNumber(i)) {
                 sum += i;
             }
