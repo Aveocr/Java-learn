345fb272f8009fc72ece4fdbde59f3c2d84c7637
Denis <aveocr@mail.ru>
2021-10-12 22:57:53 +0400

    Функции были вынесены согласно заданию, которое они выполняют

--- src/main/java/com/example/App/work2/Task2.java
+++ src/main/java/com/example/App/work2/Task2.java
@@ -5,9 +5,19 @@ import com.example.App.work2.Task1;
 public class Task2 {
     public static void main(String[] argc) {
         Task1 workWithArray = new Task1();
+        Task2 sumPositive = new Task2();
         int[] array = workWithArray.createArray(10);
         array = workWithArray.fillArrayByRandom(array, -50, 50);
         workWithArray.printArray(array);
-        System.out.print("\nThe sum of the positive elements of the array is " + workWithArray.SumPositiveElem(array));
+        System.out.print("\nThe sum of the positive elements of the array is " + sumPositive.SumPositiveElem(array));
+    }
+
+    public int SumPositiveElem(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i] <= 0 ? 0 : arr[i];   // if number is negative then sum + 0 else sum + arr[i]
+        }
+
+        return sum;
     }
 }
--- src/main/java/com/example/App/work2/Task5.java
+++ src/main/java/com/example/App/work2/Task5.java
@@ -5,12 +5,26 @@ import com.example.App.work2.Task1;
 public class Task5 {
     public static void main(String[] argc) {
         Task1 workWithArray = new Task1();
+        Task3 findMinMaxElem = new Task3();
+        Task5 averageCount = new Task5();
         int[] array = workWithArray.createArray(10);
         array = workWithArray.fillArrayByRandom(array, 10, 100);
-        int min = workWithArray.findMinIndex(array);
-        int max = workWithArray.findMaxIndex(array);
+        int min = findMinMaxElem.findMinIndex(array);
+        int max = findMinMaxElem.findMaxIndex(array);
         workWithArray.printArray(array);
-        int average = workWithArray.averageBetweenMaxMin(array, max, min);
+        int average = averageCount.averageBetweenMaxMin(array, max, min);
         System.out.print("\nThe Average is " + average);
+    }
+
+    public int averageBetweenMaxMin(int[] arr, int max_index, int min_index) {
+        if (min_index > max_index) {
+            // a=a+b-(b=a);  Меняем значение двух переменных местами
+            min_index = min_index + max_index - (max_index=min_index);
+        }
+        int sum = 0;
+        for (int i = min_index + 1; i < max_index; i++) {
+            sum += arr[i];
+        }
+        return sum / (max_index - min_index - 1);
     }
 }
