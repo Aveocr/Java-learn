b4039d9713f2c9e6edd2d5b37b641d111f7b1fb4
Denis <aveocr@mail.ru>
2021-10-12 23:01:52 +0400

    Функции были вынесены согласно заданию, которое они выполняют

--- src/main/java/com/example/App/work2/Task1.java
+++ src/main/java/com/example/App/work2/Task1.java
@@ -29,57 +29,6 @@ public class Task1 {
         }
     }
 
-    public int findMaxIndex(int[] arr){
-        int max_elem = arr[0];
-        int max_index = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > max_elem) {
-                max_elem = arr[i];
-                max_index = i;
-            }
-        }
-        return max_index;
-    }
-
-    public int findMinIndex(int[] arr) {
-        int min_elem = arr[0];
-        int min_index = 0;
-        for(int i = 0; i < arr.length; i++){
-            if (arr[i] < min_elem) {
-                min_elem = arr[i];
-                min_index = i;
-            }
-        }
-        return min_index;
-    }
-
-    public int[] replaceMinAndMax(int[] arr, int max_index, int min_index) {
-        int c = arr[max_index];
-        arr[max_index] = arr[min_index];
-        arr[min_index] = c;
-        return arr;
-    }
-
-    public int averageBetweenMaxMin(int[] arr, int max_index, int min_index) {
-        if (min_index > max_index) {
-            // a=a+b-(b=a);  Меняем значение двух переменных местами
-            min_index = min_index + max_index - (max_index=min_index);
-        }
-        int sum = 0;
-        for (int i = min_index + 1; i < max_index; i++) {
-            sum += arr[i];
-        }
-        return sum / (max_index - min_index - 1);
-    }
-
-    public int SumPositiveElem(int[] arr) {
-        int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sum += arr[i] <= 0 ? 0 : arr[i];   // if number is negative then sum + 0 else sum + arr[i]
-        }
-
-        return sum;
-    }
 }
 
 
