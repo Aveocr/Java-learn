30928a89a2b01ea452e85dff1da81df82cf80ed5
Denis <aveocr@mail.ru>
2021-11-07 18:26:31 +0400

    Added a method for check square. Fixed the sumSideDiagonalMatrix

--- src/main/java/com/example/App/work3/Task3.java
+++ src/main/java/com/example/App/work3/Task3.java
@@ -3,27 +3,30 @@ package com.example.App.work3;
 public class Task3 {
     public static void main(String[] argc) {
         Task1 mainFunction = new Task1();
-        Task2 sumRow = new Task2();
+        Task2 sumColumn = new Task2();
         Task3 sumDiagonal = new Task3();
         int[][] matrix = mainFunction.createDoubleArray(10);
         matrix = mainFunction.fillDoubleArrayByRandom(matrix, 10, 100);
-        // If main diagonal equal Said diagonal then miltiplication it; =>
-        // (15 == 15){true, 1} * sum = sum; else (14 == 15){false, 0} * sum = 0
-        int sum = ((sumDiagonal.sumMainDiagonalMatrix(matrix) == sumDiagonal.sumSideDiagonalMatrix(matrix)) ? 1 : 0)
-                * sumDiagonal.sumMainDiagonalMatrix(matrix);
-        boolean magicSquare = true;
 
         mainFunction.printMatrix(matrix);
+        System.out.printf("Is this a magic square? %s", ((sumDiagonal.checkMagicSquare(matrix) == true) ? "Yes" : "No"));
 
+    }
+    public boolean checkMagicSquare(int[][] matrix){
+        Task3 sumMainSaidDiagonal = new Task3();
+        Task2 sumColumn = new Task2();
+        Task1 sumRow = new Task1();
+        // If Main diagonal not equal Side diagonal
+        if (sumMainSaidDiagonal.sumMainDiagonalMatrix(matrix) != sumMainSaidDiagonal.sumSideDiagonalMatrix(matrix))
+            return false;
+        // if row not equal column and not equal side diagonal.
         for (int i = 0; i < matrix.length; i++) {
-            if (!(sumRow.sumRowDoubleArray(matrix, i) == mainFunction.sumColumnDoubleArray(matrix, i))
-                    & !(sumRow.sumRowDoubleArray(matrix, i) == sum)) {
-                magicSquare = false;
-                break;
+            if ((sumRow.sumRowDoubleArray(matrix, i) != sumColumn.sumColumnDoubleArray(matrix, i))
+            | (sumRow.sumRowDoubleArray(matrix, i) != sumMainSaidDiagonal.sumSideDiagonalMatrix(matrix))) {
+                return false;
             }
         }
-        System.out.printf("Is this a magic square? %b", magicSquare);
-
+        return true;
     }
 
     public int sumMainDiagonalMatrix(int[][] arrw) {
@@ -36,8 +39,10 @@ public class Task3 {
 
     public int sumSideDiagonalMatrix(int[][] arrw) {
         int sum = 0;
+        int i = 0;
         for (int j = arrw.length-1; j >= 0; j--) {
-            sum += arrw[j][j];
+            sum += arrw[i][j];
+            i += 1;
         }
         return sum;
     }
